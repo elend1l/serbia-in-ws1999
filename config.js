28173c66a99be9e4a1796fe76bf6510a2d8b85c7
elend1l <140706939+elend1l@users.noreply.github.com>
2026-09-21 00:03:11 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const SURVEY_CONFIG = {
-  googleAppsScriptUrl: "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE",
+  googleAppsScriptUrl: "https://script.google.com/macros/s/AKfycbxl2awKZaHSfT7fs5WT6J9WMi7xrOeKUevWeW8YoSBWGhk3ajlCeowISAYh7YwM1-YK/exec",
   songs: [
     {id:"song1", title:"Song 1: Ja bih te sanjala", videoUrl:"https://media.world-stage.org/ws1999rs-jbt.mov", subtitleUrl:"subtitles/ws1999rs-jbt.vtt"},
     {id:"song2", title:"Song 2: Svet tuge", videoUrl:"https://media.world-stage.org/ws1999rs-stx.mov", subtitleUrl:"subtitles/ws1999rs-stx.vtt"},
