17fc8d8defcc2ef3e8f87adbc2bb03e54abaad89
elend1l <140706939+elend1l@users.noreply.github.com>
2026-09-24 20:10:27 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,28 @@
 const SURVEY_CONFIG = {
-  googleAppsScriptUrl: "https://script.google.com/macros/s/AKfycbxl2awKZaHSfT7fs5WT6J9WMi7xrOeKUevWeW8YoSBWGhk3ajlCeowISAYh7YwM1-YK/exec",
-  songs: [
-    {id:"song1", title:"Song 1: Ja bih te sanjala", videoUrl:"https://media.world-stage.org/ws1999rs-jbt.mov", subtitleUrl:"subtitles/ws1999rs-jbt.vtt"},
-    {id:"song2", title:"Song 2: Svet tuge", videoUrl:"https://media.world-stage.org/ws1999rs-stx.mov", subtitleUrl:"subtitles/ws1999rs-stx.vtt"},
-    {id:"song3", title:"Song 3: Vreme je", videoUrl:"https://media.world-stage.org/ws1999rs-vjx.mov", subtitleUrl:"subtitles/ws1999rs-vjx.vtt"},
-    {id:"song4", title:"Song 4: Ti me ne voliš", videoUrl:"https://media.world-stage.org/ws1999rs-tmn.mov", subtitleUrl:"subtitles/ws1999rs-tmn.vtt"}
-  ]
+	googleAppsScriptUrl: "https://script.google.com/macros/s/AKfycbxl2awKZaHSfT7fs5WT6J9WMi7xrOeKUevWeW8YoSBWGhk3ajlCeowISAYh7YwM1-YK/exec",
+	songs: [{
+			id: "song1",
+			title: "Song 1: Ja bih te sanjala",
+			videoUrl: "https://media.world-stage.org/ws1999rs-jbt.mov",
+			subtitleUrl: "subtitles/ws1999rs-jbt.vtt"
+		},
+		{
+			id: "song2",
+			title: "Song 2: Svet tuge",
+			videoUrl: "https://media.world-stage.org/ws1999rs-stx.mov",
+			subtitleUrl: "subtitles/ws1999rs-stx.vtt"
+		},
+		{
+			id: "song3",
+			title: "Song 3: Vreme je",
+			videoUrl: "https://media.world-stage.org/ws1999rs-vjx.mov",
+			subtitleUrl: "subtitles/ws1999rs-vjx.vtt"
+		},
+		{
+			id: "song4",
+			title: "Song 4: Ti me ne voliš",
+			videoUrl: "https://media.world-stage.org/ws1999rs-tmn.mov",
+			subtitleUrl: "subtitles/ws1999rs-tmn.vtt"
+		}
+	]
 };
